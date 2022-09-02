54a95a838968dfeeccb86b27bd91e0d49615d183
rakesh_gupta <rakeshkatrisarai@gmail.com>
2022-09-02 08:43:51 +0530

    initial commit

---
+++ App3test/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using App3test.Models;
+
+namespace App3test.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
